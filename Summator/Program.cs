1223dd598ca38653cd235c22e1a5da97fa32d7b3
IvayloGodu <104064031+IvayloGodu@users.noreply.github.com>
2022-05-07 16:19:34 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 9, });
+            if (sum == 24)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+            double averege = Summator.Averege(new double[] { 5, 10, });
+            if (averege == 7.5)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ Summator/Summator.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Summator
+{
+    public static class Summator
+    {
+        public static long Sum(int[] arr)
+        {
+            long sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+            }
+            return sum;
+        }
+        public static double Averege(double[] arr)
+        {
+            double averege = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                averege = averege + arr[i];
+            }
+            return averege/arr.Length;
+        }      
+    }
+}
